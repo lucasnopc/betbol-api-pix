648e451bd44830d45efa7a2876322bc5e2f5ff32
Lucas Almeida <lucasnopc@gmail.com>
2022-03-06 14:03:04 -0300

    tratamento do erro na requisiçao

--- src/server.js
+++ src/server.js
@@ -16,16 +16,16 @@ app.use(express.static('static'));
 const reqGNAlready = GNRequest({
   clientID: process.env.GN_CLIENT_ID,
   clientSecret: process.env.GN_CLIENT_SECRET
-});
+})
 app.get('/', async (req, res) => {
   const value = req.query.value
   const cpf = req.query.cpf
   const name = req.query.name
-  if(typeof value == 'undefined') {
-    res.status(301).json({err: "insira umma query value: {99.00}"})
-  }
   const reqGN = await reqGNAlready
-  console.log('reqGn ', reqGN)
+
+  if (typeof value == 'undefined') {
+    res.status(301).json({ err: "insira umma query value: {99.00}" })
+  }
   const dataCob = {
     calendario: {
       expiracao: 3600
@@ -36,20 +36,31 @@ app.get('/', async (req, res) => {
     chave: 'f54efa19-a7cb-427c-8e3c-63504f506ed3',
     solicitacaoPagador: 'Transfira um pix para sua conta betbol.'
   }
-  if(cpf && name) {
+  if (cpf && name) {
     dataCob.devedor = {
       cpf: cpf,
       nome: name
     }
   }
-  const cobResponse = await reqGN.post('/v2/cob', dataCob)
-  console.log(cobResponse)
-  const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`)
-
-  res.send({ qrcodeImage: qrcodeResponse.data.imagemQrcode })
+  const funcqrcodeRespose = async () => {
+    try {
+      const cobResponse = await reqGN.post('/v2/cob', dataCob)
+      const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`)
+      res.send({ qrcodeImage: qrcodeResponse.data.imagemQrcode })
+    } catch (error) {
+      console.log('restart request ', error)
+      const reqGNAlready = GNRequest({
+        clientID: process.env.GN_CLIENT_ID,
+        clientSecret: process.env.GN_CLIENT_SECRET
+      })
+    }
+    const reqGN = await reqGNAlready
+    funcqrcodeRespose()
+  }
+funcqrcodeRespose()
 });
 
-app.get('/cobrancas', async(req, res) => {
+app.get('/cobrancas', async (req, res) => {
   const reqGN = await reqGNAlready;
 
   const cobResponse = await reqGN.get('/v2/cob?inicio=2022-03-06T05:01:35Z&fim=2022-03-22T23:59:00Z');
@@ -58,11 +69,11 @@ app.get('/cobrancas', async(req, res) => {
 });
 
 app.post('/webhook(/pix)?', (req, res) => {
- app.post(process.env.WEBHOOK_SERVER, req.body)
- res.send('200');
+  app.post(process.env.WEBHOOK_SERVER, req.body)
+  res.send('200');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
 
   console.log('Node app is running on port', app.get('port'));
 });
