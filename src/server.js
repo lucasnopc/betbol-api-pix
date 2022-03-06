18476f50dc1f607a1d0db1296ec707cb0683fae5
Lucas Almeida <lucasnopc@gmail.com>
2022-03-06 12:34:55 -0300

    added name and cpf

--- src/server.js
+++ src/server.js
@@ -17,14 +17,15 @@ const reqGNAlready = GNRequest({
   clientID: process.env.GN_CLIENT_ID,
   clientSecret: process.env.GN_CLIENT_SECRET
 });
-
 app.get('/', async (req, res) => {
   const value = req.query.value
+  const cpf = req.query.cpf
+  const name = req.query.name
   if(typeof value == 'undefined') {
     res.status(301).json({err: "insira umma query value: {99.00}"})
   }
-  console.log(value)
-  const reqGN = await reqGNAlready;
+  const reqGN = await reqGNAlready
+  console.log('reqGn ', reqGN)
   const dataCob = {
     calendario: {
       expiracao: 3600
@@ -34,10 +35,16 @@ app.get('/', async (req, res) => {
     },
     chave: 'f54efa19-a7cb-427c-8e3c-63504f506ed3',
     solicitacaoPagador: 'Transfira um pix para sua conta betbol.'
-  };
-  
-  const cobResponse = await reqGN.post('/v2/cob', dataCob);
-  const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`);
+  }
+  if(cpf && name) {
+    dataCob.devedor = {
+      cpf: cpf,
+      nome: name
+    }
+  }
+  const cobResponse = await reqGN.post('/v2/cob', dataCob)
+  console.log(cobResponse)
+  const qrcodeResponse = await reqGN.get(`/v2/loc/${cobResponse.data.loc.id}/qrcode`)
 
   res.send({ qrcodeImage: qrcodeResponse.data.imagemQrcode })
 });
@@ -45,20 +52,17 @@ app.get('/', async (req, res) => {
 app.get('/cobrancas', async(req, res) => {
   const reqGN = await reqGNAlready;
 
-  const cobResponse = await reqGN.get('/v2/cob?inicio=2021-02-15T16:01:35Z&fim=2021-02-22T23:59:00Z');
+  const cobResponse = await reqGN.get('/v2/cob?inicio=2022-03-06T05:01:35Z&fim=2022-03-22T23:59:00Z');
 
   res.send(cobResponse.data);
 });
 
 app.post('/webhook(/pix)?', (req, res) => {
-  console.log(req.body);
-  res.send('200');
+ app.post(process.env.WEBHOOK_SERVER, req.body)
+ res.send('200');
 });
 
 app.listen(app.get('port'), function() {
-  if (process.env.DYNO) {
-    console.log('This is on Heroku..!!');
-    fs.openSync('/tmp/app-initialized', 'w');
-  }
+
   console.log('Node app is running on port', app.get('port'));
 });
